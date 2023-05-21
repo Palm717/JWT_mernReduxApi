aa3330d0111046c12fc9ce636fe2bc063b9aeae7
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-05-20 22:59:26 -0400

    all routes working

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,11 +7,18 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
 
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+// calling protect middleware as first arg in routes to protect these routes from attack
+// making sure a user is signed in before a route is opened up, must pass authentication prior to entering profile / update profile features
+// is it the right token? is it the right user?
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
