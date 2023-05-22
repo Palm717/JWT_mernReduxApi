0275b4c278795e4d3070cfc6797d36c76380d845
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-05-22 15:10:59 -0400

    continuing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,18 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'; // brings in bootstrap default styles
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx';
+import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={ <App /> }>
       <Route index={ true } path='/' element={ <HomeScreen />} />
+      <Route  path='/login' element={ <LoginScreen />} />
+      <Route  path='/register' element={ <RegisterScreen />} />
     </Route>
+    
+   
   )
 )
 
