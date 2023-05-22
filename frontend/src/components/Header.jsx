5ce057439c6173807cb09bedf8fe6000593283f4
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-05-22 16:55:47 -0400

    navbar tooling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,34 +1,51 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    return (
-      <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-          <Container>
-            <LinkContainer to='/'>
-              <Navbar.Brand>MERN App</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-              <Nav className='ms-auto'>
-                <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <FaSignInAlt /> Sign In
-                  </Nav.Link>
-                </LinkContainer>
-                <LinkContainer to='/register'>
-                  <Nav.Link>
-                    <FaSignOutAlt /> Sign Up
-                  </Nav.Link>
-                </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  };
-  
-  export default Header;
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>MERN App</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item>Logout</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      <FaSignInAlt /> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link>
+                      <FaSignOutAlt /> Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
